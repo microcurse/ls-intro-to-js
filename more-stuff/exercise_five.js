330048c1d525c1abe7497160982a6a45b53d4e5d
Marc <marc.maninang@gmail.com>
2022-01-17 08:09:06 -0800

    Resubmitted answer for Exercise 5

--- more-stuff/exercise_five.js
+++ more-stuff/exercise_five.js
@@ -2,4 +2,4 @@ function doSomething(string) {
   return string.split(' ').reverse().map((value) => value.length);
 }
 
-// This converts a string into an array using spaces to delineate elements within the array. It then reverses the order of the array. From that, it creates a new array with the length of each of the strings.
+// This converts a string into an array using spaces to delineate elements within the array. It then reverses the order of the array. From that, it creates a new array with the length of each of the words.
